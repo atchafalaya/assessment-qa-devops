4cd310822f3d60f3ed76003674895f5b0417023d
James Proctor <59664392+atchafalaya@users.noreply.github.com>
2021-11-21 21:04:34 -0600

    changed node server

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -6,7 +6,7 @@ require('chromedriver')
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 beforeEach(async () => {
-    driver.get('http://localhost:127.0.0.1:3000/')
+    driver.get('http://localhost:3000/')
 })
 
 afterAll(async () => {
@@ -16,17 +16,17 @@ afterAll(async () => {
 test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
-    expect(displayed).toBe('true')
+    expect(displayed).toBe(true)
 })
 
-// test('Clicking draw button shows bot choices', async () => {
-//     const draw = await driver.findElement(By.id('choices'))
-//     const displayed = await draw.isDisplayed()
-//     expect(displayed).toBe('true')
-// })
+test('Clicking draw button shows bot choices', async () => {
+    const draw = await driver.findElement(By.id('choices'))
+    const displayed = await draw.isDisplayed()
+    expect(displayed).toBe(true)
+})
 
-// test('Add To Duo adds bot to player id', async () => {
-//     const addtoduo = await driver.findElement(By.id('playerDuo'))
-//     const displayed = await addtoduo.isDisplayed()
-//     expect(displayed).toBe('true')
-// })
+test('Add To Duo adds bot to player id', async () => {
+    const addtoduo = await driver.findElement(By.id('playerDuo'))
+    const displayed = await addtoduo.isDisplayed()
+    expect(displayed).toBe(true)
+})
